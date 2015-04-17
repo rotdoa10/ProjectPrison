f90da56edfb3f53c6f1109a224de2302f8c9b6a5
Dominik <Dominik@Dominik-PC>
2015-04-17 09:01:40 +0200

    Packages hinzugefügt,
    Database und CachedConnection eingefügt (muss noch geändert werden dass es funktioniert)

---
+++ Project Prison/src/database/CachedConnection.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author Dominik
+ */
+public class CachedConnection {
+
+    private Queue<Statement> statQueue = new LinkedList<>();
+    private Connection con = null;
+
+    public CachedConnection(Connection con) {
+        this.con = con;
+    }
+
+    public Statement getStatement() throws Exception {
+
+        if (con == null) {
+            throw new Exception("not connected");
+        }
+        if (!statQueue.isEmpty()) {
+            return statQueue.poll();
+        }
+
+        return con.createStatement();
+    }
+
+    public void releaseStatement(Statement stat) throws Exception {
+
+        if (con == null) {
+            throw new Exception("not connected");
+        }
+        statQueue.offer(stat);
+    }
+}
