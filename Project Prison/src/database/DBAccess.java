e5c293a6b9cb5d1a3f7d48ee7bc68177f9261dc8
Dominik <Dominik@Dominik-PC>
2015-04-24 09:15:32 +0200

    DBAccess addPrisnoner hinzugefügt

--- Project Prison/src/database/DBAccess.java
+++ Project Prison/src/database/DBAccess.java
@@ -59,62 +59,25 @@ public class DBAccess {
         return pr;
     }    
 
-
-//    public boolean containsBook(LinkedList<Book> list, String isbn) {
-//        for (Book b : list) {
-//            if (b.getIsbn().equals(isbn)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//  
-//    public void addPrisoner(int ID, String vorname, String nachname, Date gebDate, Date inDate, Date outDate, int priority) throws Exception
-//    {
-//        Statement stat = db.getStatement();
-//        String sqlString;
-//        
-//        sqlString = "SELECT p.publisher_id, p.url "
-//                  + "FROM publishers p "
-//                  + "WHERE LOWER(p.name)=LOWER('"+pub+"')";
-//        ResultSet rs = stat.executeQuery(sqlString);
-//        rs.next();
-//        String pub_id = rs.getString("publisher_id");
-//        String url = rs.getString("url");
-//        
-//        sqlString = "SELECT * "
-//                  + "FROM authors "
-//                  + "WHERE name ='"+autor+"'";
-//        
-//        rs = stat.executeQuery(sqlString);
-//        rs.next();
-//        
-//        String aut_id;
-//        
-//        if(rs.equals(null))
-//        {
-//            aut_id = autor.substring(0, 3).trim().toUpperCase();
-//            sqlString = "INSERT INTO authors(name, author_id) "
-//                      + "VALUES('"+autor+", '"+ aut_id +"')";
-//            rs = stat.executeQuery(sqlString);
-//        }
-//        else
-//        {
-//            aut_id = rs.getString("author_id");
-//        }
-//        
-//        
-//        
-//        sqlString = "INSERT INTO booksauthors(isbn, author_id) "
-//                  + "VALUES("+isbn+", UPPER("+ aut_id +"))";
-//        rs = stat.executeQuery(sqlString);
-//        
-//        
-//        
-//        sqlString = "INSERT INTO books(title, isbn, publisher_id, url, price) "
-//                  + "VALUES('"+title+"', "+isbn+", "+pub_id+", '"+url+"/"+title+"')";
-//        rs = stat.executeQuery(sqlString);
-//    }
+    public void addPrisoner(String vorname, String nachname, Date gebDate, Date inDate, Date outDate, int priority) throws Exception
+    {
+        Statement stat = db.getStatement();
+        String sqlString;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        
+       
+        sqlString = "INSERT INTO prisoner(prID, vorname, nachname, gebDate, inDate, outDate, priority) "
+                + "VALUES(nextval('sql_pID')"
+                + ","+ vorname 
+                + ","+nachname+", "
+                + "TO_DATE('"+ sdf.format(gebDate)+",'dd.MM.yyyy'), "
+                + "TO_DATE('"+ sdf.format(inDate)+",'dd.MM.yyyy'),"
+                + "TO_DATE('"+ sdf.format(gebDate)+",'dd.MM.yyyy'),"
+                + ""+priority+"')";
+        
+        ResultSet rs = stat.executeQuery(sqlString);
+        rs = stat.executeQuery(sqlString);
+    }
 
     public static void main(String[] args) throws IOException, FileNotFoundException, ClassNotFoundException, SQLException, Exception {
         DBAccess dba = new DBAccess();     
