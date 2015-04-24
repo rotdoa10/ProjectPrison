7442dd5145bdd28e6d33c70c053eb5bf358e3693
Dominik <Dominik@Dominik-PC>
2015-04-24 08:22:39 +0200

    Prisoner Constructor Hinzugefügt Getter/Setter hinzugefügt

--- Project Prison/src/beans/prisoner.java
+++ Project Prison/src/beans/prisoner.java
@@ -19,6 +19,63 @@ public class prisoner
     Date inDate;
     Date outDate;
     int priority;
+
+    public prisoner(String vorname, String nachname, Date gebDate, Date inDate, Date outDate, int priority) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.gebDate = gebDate;
+        this.inDate = inDate;
+        this.outDate = outDate;
+        this.priority = priority;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public Date getGebDate() {
+        return gebDate;
+    }
+
+    public void setGebDate(Date gebDate) {
+        this.gebDate = gebDate;
+    }
+
+    public Date getInDate() {
+        return inDate;
+    }
+
+    public void setInDate(Date inDate) {
+        this.inDate = inDate;
+    }
+
+    public Date getOutDate() {
+        return outDate;
+    }
+
+    public void setOutDate(Date outDate) {
+        this.outDate = outDate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
     
     
 }
