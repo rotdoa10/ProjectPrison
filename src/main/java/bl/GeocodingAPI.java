6d4e9a8e7d9ee4c3edc1c5aea343382702b5824b
koldaa10 <koldaa10@htlkaindorf.at>
2015-07-06 11:05:36 +0200

    GeocodingAPI-Klasse erzeugt David

---
+++ src/main/java/bl/GeocodingAPI.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bl;
+
+/**
+ *
+ * @author David
+ */
+public class GeocodingAPI {
+    
+    
+    /*
+        Die Methode findet zum eingegeben Ort die passenden
+        x- und y- Koordinaten und liefert diese in einem 
+        double-Feld zurück
+        Beispiel-Link: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
+    */
+    public double[] OrtToKoord(String name)
+    {
+        double[] koordinaten = new double[2];
+        
+        
+        
+        
+        return koordinaten;
+    }
+    
+}
