df893c29db880999dd3306c9493d38e8de7c6941
goevea11 <goevea11@htlkaindorf.at>
2015-07-08 10:19:14 +0200

    Koord To Ort

--- src/main/java/bl/GeocodingAPI.java
+++ src/main/java/bl/GeocodingAPI.java
@@ -47,12 +47,14 @@ public class GeocodingAPI {
     }
     public String KoordToOrt(double[] koordinaten)
     {
+        String requestUrl="https://maps.googleapis.com/maps/api/geocode/xml?latlng="+koordinaten[0]+","+koordinaten[1]+"&key="+apiKey;
+        
         try {
-            //https://maps.googleapis.com/maps/api/geocode/json?address=Ligist&key=AIzaSyDI6ex1fUOJKjomDnoe97atKcWyxDotOEo
-
-            SendToMapsAPI texturl = new SendToMapsAPI("https://maps.googleapis.com/maps/api/geocode/json?address=Ligist&key=AIzaSyDI6ex1fUOJKjomDnoe97atKcWyxDotOEo");
+            SendToMapsAPI sendObject = new SendToMapsAPI(requestUrl);
+            String answer = sendObject.read();
+            System.out.println(answer);
         } catch (MalformedURLException ex) {
-            Logger.getLogger(GeocodingAPI.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Fehler beim Konvertieren des Ortes zu Koordinaten");
         }
         return "";
     }
@@ -60,5 +62,7 @@ public class GeocodingAPI {
     public static void main(String[] args) {
         GeocodingAPI api = new GeocodingAPI();
         api.OrtToKoord("Ligist");
+        double[] k = {46.9917246, 15.2107184};
+        api.KoordToOrt(k);
     }
 }
