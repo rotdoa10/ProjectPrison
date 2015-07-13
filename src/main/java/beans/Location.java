fcce42213a0380ad52934cf15c42e3ecc59fdefd
goevea11 <goevea11@htlkaindorf.at>
2015-07-13 11:16:11 +0200

    Radiobuttons

--- src/main/java/beans/Location.java
+++ src/main/java/beans/Location.java
@@ -15,7 +15,23 @@ public class Location {
     private String name;
     private double xKoord;
     private double yKoord;
+    private double hoehe;
 
+    public double getHoehe() {
+        return hoehe;
+    }
+
+    public void setHoehe(double hoehe) {
+        this.hoehe = hoehe;
+    }
+
+    public Location(String name, double xKoord, double yKoord, double hoehe) {
+        this.name = name;
+        this.xKoord = xKoord;
+        this.yKoord = yKoord;
+        this.hoehe = hoehe;
+    }
+    
     public Location(String name, double xKoord, double yKoord) {
         this.name = name;
         this.xKoord = xKoord;
