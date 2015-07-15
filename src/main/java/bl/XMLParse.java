31b684cffeee21978cf873a05ba1d36c0a6db3ae
neupaa11 <patzineubi@192.168.1.19>
2015-07-14 19:59:43 -0700

    Waypoints mir Directions API - soweit fertig

--- src/main/java/bl/XMLParse.java
+++ src/main/java/bl/XMLParse.java
@@ -144,48 +144,64 @@ public class XMLParse {
             NodeList duration = elem.getElementsByTagName("duration");
             NodeList distance = elem.getElementsByTagName("distance");
             NodeList start_loc = elem.getElementsByTagName("start_location");
-            NodeList html_instruction = elem.getElementsByTagName("html_instruction");
+            NodeList html_instruction = elem.getElementsByTagName("html_instructions");
             NodeList polyline = elem.getElementsByTagName("polyline");
    
             for (int k = 0; k < distance.getLength(); k++) {
 
+                //
                 Element elem_end_loc = (Element) end_loc.item(k);
                 Element elem_distance = (Element) distance.item(k);
                 Element elem_duration = (Element) duration.item(k);
                 Element elem_start_loc = (Element) start_loc.item(k);
                 Element travmode = (Element) travel_mode.item(k);
+                //
                 NodeList end_loc_lat = elem_end_loc.getElementsByTagName("lat");
                 NodeList end_loc_lng = elem_end_loc.getElementsByTagName("lng");
                 Element endloclat = (Element) end_loc_lat.item(k);
                 Element endloclng = (Element) end_loc_lng.item(k);
+                //
                 NodeList distance_value = elem_distance.getElementsByTagName("value");
                 Element elem_distance_value = (Element) distance_value.item(k);
                 NodeList duration_value = elem_duration.getElementsByTagName("value");
                 Element elem_duration_value = (Element) duration_value.item(k);
+                //
                 NodeList start_loc_lat = elem_start_loc.getElementsByTagName("lat");
                 NodeList start_loc_lng = elem_start_loc.getElementsByTagName("lng");
                 Element startloclat = (Element) start_loc_lat.item(k);
                 Element startloclng = (Element) start_loc_lng.item(k);
+                
                 Element html_instr = (Element) html_instruction.item(k);
+                //
                 Element polylinepoint = (Element) polyline.item(k);
+                //
                 String str_distance = elem_distance_value.getTextContent();
                 float f_distance = Float.parseFloat(str_distance);
+                
                 String str_duration = elem_duration_value.getTextContent();
                 int i_duration = Integer.parseInt(str_duration);
+                //
                 String str_endloclat = endloclat.getTextContent();
                 float f_endloclat = Float.parseFloat(str_endloclat);
+                
                 String str_endloclng = endloclng.getTextContent();
                 float f_endloclng = Float.parseFloat(str_endloclng);
+                //
                 String str_travelmode = travmode.getTextContent();
+                //
                 String str_startloclat = startloclat.getTextContent();
                 float f_startloclat = Float.parseFloat(str_startloclat);
+                
                 String str_strloclng = startloclng.getTextContent();
                 float f_strloclng = Float.parseFloat(str_strloclng);
+                
                 String str_html_instruction = html_instr.getTextContent();
+                
                 String str_polylinepoint = polylinepoint.getTextContent();
                 
-                list.add(new Leg(f_distance, i_duration, f_endloclat, f_endloclng, str_html_instruction, str_polylinepoint,f_startloclat,f_strloclng,str_travelmode));
                 
+                list.add(new Leg(f_distance, i_duration, f_endloclat, f_endloclng, str_html_instruction, str_polylinepoint,f_startloclat,f_strloclng,str_travelmode));
+//                
             }
         }
 
